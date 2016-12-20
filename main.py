aabdf1b5fa78b34b0f16a598fb9906b86ec7f535
Rasmus Elvborn Jensen <xxxstriker666xxx@gmai.com>
2016-12-20 22:02:21 +0100

    First Commit

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from file_manager import *
 from store_manager import Store
 
 def collect_data():
-    fakta = Store("fakta", "http://www.fakta.dk/tilbudsavis/");
+    fakta = Store("fakta", "http://www.fakta.dk/tilbudsavis/")
+
+    fakta.get_data_fakta()
 
 collect_data()
